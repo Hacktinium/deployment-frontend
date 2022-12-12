efa419df9b0b952725f5258a574e3c674a0b76e7
Steve Beecheno <iamhacktinium@gmail.com>
2022-12-12 23:28:17 +0000

    Base styling complete

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,13 +4,13 @@ const Card = ({ title, thumbnail, link }) => {
 	return (
 		<a href={link} className="project-link">
 			<div className="Card">
+				<div className="glass col">
+					<div className="info col">
+						<h3>{title}</h3>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dolores cumque eius voluptatum qui quos cupiditate odit perferendis ab magni?</p>
+					</div>
+				</div>
 				<img className="card-img" alt={title} src={thumbnail} />
-				<div className="glass"></div>
-				<div className="shadow"></div>
-				<article className="info">
-					<h3>{title}</h3>
-					<p>description</p>
-				</article>
 			</div>
 		</a>
 	);
--- src/db/db.js
+++ src/db/db.js
@@ -1,24 +1,24 @@
 const projects = [
 	{
-		title: "JS PokeDex",
+		title: "JS PokéDex",
 		link: "https://hacktinium.github.io/pokedex/",
 		thumbnail: require('../assets/pokedex_card_thumbnail.png'),
 		id: 1,
 	},
 	{
-		title: "JS PokeDex",
+		title: "JS PokéDex",
 		link: "https://hacktinium.github.io/pokedex/",
 		thumbnail: require('../assets/pokedex_card_thumbnail.png'),
 		id: 2,
 	},
 	{
-		title: "JS PokeDex",
+		title: "JS PokéDex",
 		link: "https://hacktinium.github.io/pokedex/",
 		thumbnail: require('../assets/pokedex_card_thumbnail.png'),
 		id: 3,
 	},
 	{
-		title: "JS PokeDex",
+		title: "JS PokéDex",
 		link: "https://hacktinium.github.io/pokedex/",
 		thumbnail: require('../assets/pokedex_card_thumbnail.png'),
 		id: 4,
